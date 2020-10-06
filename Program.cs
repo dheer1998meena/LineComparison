4414a74083f0056614da41ce12f9f31d3ba1834c
dheer meena <dheer1998meena@gmail.com>
2020-10-06 17:11:36 +0530

    [Dheer] Add . UC1_CheckEqualityoOfTwoLine

--- Program.cs
+++ Program.cs
@@ -6,15 +6,29 @@ namespace LineComparison
     {
         static void Main(string[] args)
         {
-            int X1, X2, Y1, Y2;
-            Random radom = new Random();
-            X1 = radom.Next(-100, 100);
-            X2 = radom.Next(-100, 100);
-            Y1 = radom.Next(-100, 100);
-            Y2 = radom.Next(-100, 100);
-            Console.WriteLine("The coordinates are : " + X1 + " " + Y1 + " " + X2 + " " + Y2);
-            double length = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
-            Console.WriteLine("Length of the line : " + Math.Round(length, 2));
+            double[] length = new double[2];
+            int count = 0;
+            while (count < 2)
+            {
+                int X1, X2, Y1, Y2;
+                Random radom = new Random();
+                X1 = radom.Next(-100, 100);
+                X2 = radom.Next(-100, 100);
+                Y1 = radom.Next(-100, 100);
+                Y2 = radom.Next(-100, 100);
+                Console.WriteLine("The coordinates are : (" + X1 + " " + Y1 + ") (" + X2 + " " + Y2+")");
+                length [count] = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+                Console.WriteLine("Length of the line : " + Math.Round(length[count], 2));
+                count++;
+            }
+            if (length[0]==length[1])
+            {
+                Console.WriteLine("Lines are equals :");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not equals");
+            }
         }
     }
 }
